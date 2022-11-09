8bbef101a41c6a5d60bf05333768aa33ea0db314
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-11-09 15:25:14 +0000

    Display the board
    Print the board to the console using simple cout commands.

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -3,4 +3,14 @@
 int main()
 {
   std::cout << "Welcome to tic-tac-toe!\n";
+
+  std::cout << "     |     |     \n";
+  std::cout << "     |     |     \n";
+  std::cout << "_____|_____|_____\n";
+  std::cout << "     |     |     \n";
+  std::cout << "     |     |     \n";
+  std::cout << "_____|_____|_____\n";
+  std::cout << "     |     |     \n";
+  std::cout << "     |     |     \n";
+  std::cout << "     |     |     \n";
 }
