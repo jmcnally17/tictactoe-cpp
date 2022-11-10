024daae31f91bc222fdefc4f0a3de2742e472b92
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-11-10 13:27:44 +0000

    Shorten valid cell conditional

--- src/CheckIfCellIsValid.cpp
+++ src/CheckIfCellIsValid.cpp
@@ -4,11 +4,7 @@
 void checkIfCellIsValid(std::string board[3][3], int a, int b, int &playerTurn)
 {
   if (board[a][b] != "X" && board[a][b] != "O")
-  {
     fillCell(board, a, b, playerTurn);
-  }
   else
-  {
     std::cout << "That cell is already taken!\n";
-  }
 }
