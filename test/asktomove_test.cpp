1844698fc4812609641b87b3a04c735687a55028
jmcnally17 <joe.t.mcnally@hotmail.com>
2022-11-11 22:55:12 +0000

    Test askToMove
    Create a new test file that checks the correct output is sent to cout,
    depending on the value of playerTurn.
    
    In order to test this, pass a reference to an ostream variable into the
    function with a default value of std::cout. This will allow an
    ostringstream variable to be passed into the function call in the test
    which can then be checked to see if it contains the correct string
    message.

---
+++ test/asktomove_test.cpp
@@ -0,0 +1,19 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include "../src/ask_to_move.cpp"
+
+TEST_CASE("askToMove prints a message to player 1 in the console if playerTurn is equal to 1")
+{
+  int playerTurn = 1;
+  std::ostringstream oss;
+  askToMove(playerTurn, oss);
+  CHECK(oss.str() == "\nPlayer 1, please pick a cell from 1 to 9:\n");
+}
+
+TEST_CASE("askToMove prints a message to player 2 in the console if playerTurn is equal to 2")
+{
+  int playerTurn = 2;
+  std::ostringstream oss;
+  askToMove(playerTurn, oss);
+  CHECK(oss.str() == "\nPlayer 2, please pick a cell from 1 to 9:\n");
+}
